56c6713bd3d2dc4b3105691fdecb63338f045a27
calderonsin <juccalderondo@unal.edu.co>
2018-09-16 17:12:58 -0500

    2 test

---
+++ src/AllTest.java
@@ -0,0 +1,92 @@
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class AllTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+	//Given a number of Data I want to put in my list
+	//When I created the list
+	//Then I expected the size of the list to be equal to the number of data that I want.
+	@Test
+	public void Ntest() throws FileNotFoundException {
+		
+		List<Student> studentList = new ArrayList<Student>();
+		Scanner in = new Scanner(Solution.file);		
+		int esperado = Integer.parseInt(in.nextLine());
+		studentList= Solution.CreateList(in,esperado);
+		int resultado=studentList.size();
+		
+		assertEquals(esperado, resultado);
+	}
+	
+	@Test
+	//Given a list of student which has two students with the same CGPA
+	//When I sort the list
+	//Then I expected that my program sort the two same CGPA with their name in an alphabetical order.
+	public void sameCGPA() throws FileNotFoundException {
+		
+		List<Student> studentList = new ArrayList<Student>();
+		String   fileName = "C:\\Users\\Calde\\Desktop\\QAhomework\\src\\Test2.txt";
+		File test2  = new File(fileName);		
+		Scanner in2 = new Scanner(test2);		
+		int testCases = Integer.parseInt(in2.nextLine());
+		studentList= Solution.CreateList(in2,testCases);
+		List<String> resultado= Solution.printStudents(studentList);
+		List<String> esperado= new ArrayList<String>();
+		esperado.add("Ashis");
+		esperado.add("Samara");
+		esperado.add("Samiha");
+		esperado.add("Fahim");
+		esperado.add("Rumpa");
+		
+		assertEquals(esperado, resultado);
+	}
+	
+public void sameCGPAName() throws FileNotFoundException {
+		
+		List<Student> studentList = new ArrayList<Student>();
+		String   fileName = "C:\\Users\\Calde\\Desktop\\QAhomework\\src\\Test3.txt";
+		File test3  = new File(fileName);		
+		Scanner in3 = new Scanner(test3);		
+		int testCases = Integer.parseInt(in3.nextLine());
+		studentList= Solution.CreateList(in3,testCases);
+		List<String> resultado= Solution.printStudents(studentList);
+		List<String> esperado= new ArrayList<String>();
+		esperado.add("Ashis");
+		esperado.add("Samara");
+		esperado.add("Samiha");
+		esperado.add("Fahim");
+		esperado.add("Rumpa");
+		
+		assertEquals(esperado, resultado);
+	}
+	
+	
+	
+
+}
--- src/Solution.java
+++ src/Solution.java
@@ -5,31 +5,45 @@ import java.util.*;
 //Complete the code
 public class Solution
 {
+	static String   fileName = "C:\\Users\\Calde\\Desktop\\QAhomework\\src\\Test3.txt";
+	public static   File file  = new File(fileName);
+	
+
+public static List<Student> CreateList( Scanner in, int testCases){
+	List<Student> studentList = new ArrayList<Student>();
+	while(testCases>0){
+		int id = in.nextInt();
+		String fname = in.next();
+		double cgpa = in.nextDouble();		
+		Student st = new Student(id, fname, cgpa);
+		studentList.add(st);		
+		testCases--;
+	}
+	
+	
+	return studentList;
+	
+}
+public static List<String> printStudents(List<Student> studentList) {
+	List<String> ordenado = new ArrayList<String>();
+	Collections.sort(studentList);	
+	for(Student st: studentList){		
+		ordenado.add(st.getFname());
+	System.out.println(st.getFname()+ " " + st.getId());
+	
+}	return ordenado;}
+
+	
 	public static void main(String[] args) throws FileNotFoundException{
 		
-		String fileName = "C:\\Users\\Calde\\Desktop\\java\\src\\input00.txt";
+		
 		//Scanner in = new Scanner(System.in);
-		File file = new File(fileName);
+		
 		Scanner in = new Scanner(file); 
-		int testCases = Integer.parseInt(in.nextLine());
+		int testCases = Integer.parseInt(in.nextLine());		
 		
-		List<Student> studentList = new ArrayList<Student>();
-		while(testCases>0){
-			int id = in.nextInt();
-			String fname = in.next();
-			double cgpa = in.nextDouble();
-			
-			Student st = new Student(id, fname, cgpa);
-			studentList.add(st);
-			
-			testCases--;
-		}
-		
-		 Collections.sort(studentList);
-		
-      	for(Student st: studentList){
-      		
-			System.out.println(st.getFname());
-		}
+		List<Student> studentList=CreateList(in,testCases);	 
+		 printStudents(studentList);
+      	
 	}
 }
